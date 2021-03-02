ae02db31b28b039cc2123d273384642e27fdbd3b
Valentin Weber <vweber@stud.hs-heilbronn.de>
2021-03-02 23:10:14 +0100

    initial commit

---
+++ mendeley_watchdog/__init__.py
@@ -0,0 +1,6 @@
+"""
+Distributes Mendeley bibtex files to specific directories upon change
+
+:copyright: (c) 2021 by Valentin Weber
+:license:
+"""
---
+++ mendeley_watchdog/main.py
@@ -0,0 +1,9 @@
+"""Main entry point for mendeley-watchdog"""
+
+
+def main() -> None:
+    """Main entry point for mendeley-watchdog"""
+    
+
+if __name__ == "__main__":
+    main()
